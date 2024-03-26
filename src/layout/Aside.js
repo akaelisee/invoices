5396757e50d01024e0f28e984f831f393dd2d430
Jean-Élisée Aka <akaje@paf.publications-agora.fr>
2024-03-26 16:55:54 +0100

    first commit

---
+++ src/layout/Aside.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './aside.css'
+
+const Aside = () => {
+    return (
+        <div className='inner-aside'>
+            Aside
+            <div className='account'>
+
+            </div>
+        </div>
+    );
+}
+
+export default Aside
